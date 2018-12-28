23a5aeaa6602c84ae7bad33952c8dc88220e10f8
User <User@DESKTOP-QMJ9AG0.home>
2018-12-28 23:56:36 +0100

    checkbox madness

--- src/main/resources/static/js/app/App.js
+++ src/main/resources/static/js/app/App.js
@@ -1,14 +1,139 @@
-function Answer(props) {
-	var keys = Object.keys(props.map);
+//function Answers(props) {
+//let keys = Object.keys(props.currentQuestion.data.answers);
+//let selectionForThisQuestion = props.givenAnswers.get(props.currentQuestion.id);
+//return <ul>
+//{keys.map(key => (
+//<Checkbox id={key} checked={selectionForThisQuestion && selectionForThisQuestion.includes(key)} text={props.currentQuestion.data.answers[key]}/>
+//))}
+//</ul>;
+//}
+
+function Answers(props) {
+	let keys = Object.keys(props.currentQuestion.data.answers);
+	let selectionForThisQuestion = props.givenAnswers.get(props.currentQuestion.id);
 	return <ul>
 	{keys.map(key => (
-			<li>
-			<input type='checkbox' className={'form-check-input'} id={key} />
-			<label className={'form-check-label'} for={key}>{props.map[key]}</label>
-			</li>
+			<Checkbox id={key} checked={selectionForThisQuestion && selectionForThisQuestion.includes(key)} text={props.currentQuestion.data.answers[key]}/>
 	))}
 	</ul>;
 }
+
+
+//<input type='checkbox' className={'form-check-input'} id={key} defaultChecked={selectionForThisQuestion && selectionForThisQuestion.includes(key)}/>
+//<label className={'form-check-label'} for={key}>{props.currentQuestion.data.answers[key]}</label>
+
+//function Answers(props) {
+//return (<ul>
+//<li>
+//<input type='checkbox' className={'form-check-input'} id={props.id} checked={props.checked}/>
+//<label className={'form-check-label'} for={props.id}>id == {props.id}</label>
+//</li></ul>);
+//}
+
+class Checkbox extends React.Component {
+	constructor(props) {
+		super(props);
+//		this.state = {
+//				isChecked: false,
+//		};
+	}
+	toggleChange = () => {
+//		if (this.state.isChecked)
+		if (this.props.checked)
+			this.props.unCheckAnswer(this.props.id);
+		else
+			this.props.checkAnswer(this.props.id);
+
+//		this.setState({
+//			isChecked: !this.state.isChecked,
+//		});
+	}
+//	componentDidUpdate(prevProps) {
+//	// Typical usage (don't forget to compare props):
+//	if (this.props.exam !== prevProps.exam) {
+//	this.fetchData();
+//	}
+//	}
+	render() {
+		return (
+
+				<li>
+				<input type='checkbox' className={'form-check-input'} id={this.props.id} checked={this.props.checked} onChange={this.toggleChange}/>
+				<label className={'form-check-label'} for={this.props.id}>{this.props.text}</label>
+				</li>
+		);
+	}
+}
+
+//function Checkbox(props) {
+//return (
+//<li>
+//<input type='checkbox' className={'form-check-input'} id={props.id} defaultChecked={props.checked}/>
+//<label className={'form-check-label'} for={props.id}>{props.text}</label>
+//</li>);
+//}
+/*
+class Answers extends React.Component {
+
+	constructor(props) {
+		super(props);
+		this.state = {
+				currentQuestion: this.props.currentQuestion,
+				givenAnswers:  this.props.givenAnswers
+		};
+	}
+
+	componentDidUpdate(prevProps) {
+		// Typical usage (don't forget to compare props):
+		if (this.props.currentQuestion.id !== prevProps.currentQuestion.id) {
+			this.setState({
+				currentQuestion: this.props.currentQuestion,
+				givenAnswers: this.props.givenAnswers
+			});
+		}
+	}
+
+	render() {
+		let keys = Object.keys(this.state.currentQuestion.data.answers);
+		let selectionForThisQuestion = this.state.givenAnswers.get(this.state.currentQuestion.id);
+		return <ul>
+		{keys.map(key => (
+				<li>
+				<input type='checkbox' className={'form-check-input'} id={key} defaultChecked={selectionForThisQuestion && selectionForThisQuestion.includes(key)}/>
+				<label className={'form-check-label'} for={key}>{this.state.currentQuestion.data.answers[key]}</label>
+				</li>
+		))}
+		</ul>;
+	}
+
+
+}*/
+
+function ButtonRow(props){
+	let prevButton;
+	let subButton = <button className={"btn btn-secondary"} onClick={props.submit}>Submit</button>;
+	let nextButton;
+	if (props.showPrevious){
+		prevButton = <button className={"btn btn-secondary float-left"} onClick={props.previous}>Previous</button>;
+	}
+	if (props.showNext){
+		nextButton = <button className={"btn btn-secondary float-right"} onClick={props.next}>Next</button>;
+	}
+	return (
+			<div className={"form-row"}>
+			<div className={"col"}>
+			{prevButton}
+			</div>
+			<div className={"col"}>
+			{subButton}
+			</div>
+			<div className={"col"}>
+			{nextButton}
+			</div>
+			</div>
+	);
+}
+
 
 
 class QuestionPanel extends React.Component {
@@ -20,10 +145,15 @@ class QuestionPanel extends React.Component {
 				error: null,
 				questions: [],
 				currentQuestion: null,
-				index: 0
+				index: 0,
+				givenAnswers: new Map()
 		};
 		this.next = this.next.bind(this);
 		this.previous = this.previous.bind(this);
+		this.submit = this.submit.bind(this);
+		this.checkAnswer = this.checkAnswer.bind(this);
+		this.unCheckAnswer = this.unCheckAnswer.bind(this);
+
 	}
 
 
@@ -50,8 +180,50 @@ class QuestionPanel extends React.Component {
 		);
 	}
 
-	next() {
+	checkAnswer(answerId) {
+		let newGivenAnswers = this.state.givenAnswers;
+		
+		if (newGivenAnswers.has(this.state.currentQuestion.id))
+			newGivenAnswers.get(this.state.currentQuestion.id).push(answerId);
+		else
+			newGivenAnswers.set(this.state.currentQuestion.id, [answerId]);
+		
 		this.setState({
+			givenAnswers: newGivenAnswers,
+		});
+
+	}
+
+	unCheckAnswer(answerId) {
+		let newGivenAnswers = this.state.givenAnswers;
+
+		let asnwersForQUestion = newGivenAnswers.get(this.state.currentQuestion.id);
+		let index = asnwersForQUestion.indexOf(answerId);
+
+		if (index > -1) {
+			asnwersForQUestion.splice(index, 1);
+		}
+		
+		this.setState({
+			givenAnswers: newGivenAnswers,
+		});
+	}
+
+	next() {
+
+//		let selectedOption = []
+
+//		let checked = document.querySelectorAll('input[type=checkbox]:checked');
+
+//		checked.forEach(function(e){
+//		selectedOption.push(e.id)
+//		});
+
+//		let newGivenAnswers = this.state.givenAnswers;
+//		newGivenAnswers.set(this.state.currentQuestion.id, selectedOption);
+
+		this.setState({
+//			givenAnswers: newGivenAnswers,
 			index: this.state.index + 1,
 			currentQuestion: this.state.questions[this.state.index + 1]
 		});
@@ -64,31 +236,46 @@ class QuestionPanel extends React.Component {
 		});
 	}
 
+	submit() {
+
+	}
+
 	render() {
-		const { error, isLoaded, questions, currentQuestion } = this.state;
+		const { error, isLoaded, questions, currentQuestion, givenAnswers, index } = this.state;
 		if (error) {
 			return <div>Error: {error.message}</div>;
 		} else if (!isLoaded) {
 			return <div>Loading...</div>;
 		} else {
+			let answerListNode = document.getElementById('answerList');
+			if (answerListNode)
+				ReactDOM.unmountComponentAtNode(document.getElementById('answerList'));
 			return (
 					<fieldset>
 					<div className={"form-group"}>
 					<textarea className={"form-control"} rows="10" value={currentQuestion.data.question} readonly></textarea>
 					</div>
-					<div className={"form-group"}>
-					<Answer map={currentQuestion.data.answers} />
+					<div className={"form-group"} id="answerList">
+					<ul>
+					{Object.keys(currentQuestion.data.answers).map(key => (
+							<Checkbox id={key} checked={givenAnswers.get(currentQuestion.id) && givenAnswers.get(currentQuestion.id).includes(key)} text={currentQuestion.data.answers[key]} checkAnswer={this.checkAnswer} unCheckAnswer={this.unCheckAnswer}/>
+					))}
+					</ul>
 					</div>
 					<hr />
-					<div className={"form-group"}>
-					<button className={"btn btn-secondary"} onClick={this.previous}>Previous</button>
-					<button className={"btn btn-secondary float-right"} onClick={this.next}>Next</button>
-					</div>
+					<ButtonRow previous={this.previous} next={this.next} submit={this.submit} showNext={index<questions.length-1} showPrevious={index>0}/>
 					</fieldset>
 			);
 		}
 	}
 }
+
+//<Checkbox id={key} checked={givenAnswers.get(currentQuestion.id) && givenAnswers.get(currentQuestion.id).includes(key)} text={currentQuestion.data.answers[key] checkAnswer={this.checkAnswer}/>
+
+
+//<Answers currentQuestion={currentQuestion} givenAnswers={givenAnswers}/>
+
+//<Answers id={currentQuestion.id} checked={currentQuestion.id == 1}/>
 
 class CategorySelection extends React.Component {
 	constructor(props) {
@@ -106,7 +293,7 @@ class CategorySelection extends React.Component {
 	componentDidMount() {
 		this.fetchData();
 	}
-	
+
 	fetchData() {
 		fetch("http://localhost:8080/questions/categories?exam=" + this.props.exam)
 		.then(res => res.json())
@@ -126,13 +313,13 @@ class CategorySelection extends React.Component {
 				}
 		);
 	}
-	
+
 	componentDidUpdate(prevProps) {
-		  // Typical usage (don't forget to compare props):
-		  if (this.props.exam !== prevProps.exam) {
-		    this.fetchData();
-		  }
+		// Typical usage (don't forget to compare props):
+		if (this.props.exam !== prevProps.exam) {
+			this.fetchData();
 		}
+	}
 
 	render() {
 		const { error, isLoaded, categories } = this.state;
@@ -193,7 +380,7 @@ class SelectQuiz extends React.Component {
 		);
 
 	}
-	
+
 
 	onExamChange() {
 		const select = this.examRef.current;
@@ -201,14 +388,14 @@ class SelectQuiz extends React.Component {
 			exam: select.value
 		});
 	}
-	
+
 	onCategoryChange() {
 		const select = this.categoryRef.current;
 		this.setState({
 			category: select.value
 		});
 	}
-	
+
 	onStart() {
 		const exam = this.examRef.current.value;
 		const category = this.categoryRef.current.value;
@@ -260,7 +447,7 @@ class App extends React.Component {
 		this.start = this.start.bind(this);
 
 	}
-	
+
 	start(exam, category) {
 		this.setState({
 			exam: exam,
